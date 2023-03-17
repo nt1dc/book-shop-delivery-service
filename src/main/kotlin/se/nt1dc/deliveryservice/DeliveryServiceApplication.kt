17dd949312417d0871ae7439e8b58c5bca35874c
nt1dc <nt1dcare@gmail.com>
2023-03-17 13:45:48 +0300

    init modules

---
+++ src/main/kotlin/se/nt1dc/deliveryservice/DeliveryServiceApplication.kt
@@ -0,0 +1,11 @@
+package se.nt1dc.deliveryservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeliveryServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeliveryServiceApplication>(*args)
+}
